f56cbaf8e96f1da9feb69e6aef3bc3e3749235c1
Eyvette Thompson <eyvettet@gmail.com>
2017-10-05 22:55:59 -0400

    Add files via upload

---
+++ chapter__3_lab.php
@@ -0,0 +1,25 @@
+<?php
+
+$firstName = "Eyvette";
+$yearOfBirth = 1969;
+$height = 5.8;
+$blueEyes = false;
+$millionare = null;
+
+print("First Name: " . $firstName);
+print("<br/>");
+
+print("Birth Year: " . $yearOfBirth);
+print("<br/>");
+
+print("My Height: " . $height);
+print("<br/>");
+
+print("Have Blue Eyes: " . $blueEyes);
+print("<br/>");
+
+print("The Millionare in the Family: " . $millionare);
+print("<br/>");
+
+
+?>
